810bffee96ed6e8b62decd070508c2fae21612b6
Tom Coward <tom@versobit.com>
2020-05-28 00:50:55 +0100

    Set `ticket_id` of associated resource to 0 if ticket is deleted

---
+++ NF/Tickets/Service/Ticket/Deleter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace VersoBit\ResourceTickets\NF\Tickets\Service\Ticket;
+
+use http\Params;
+
+class Deleter extends XFCP_Deleter
+{
+    public function delete($type, $reason = '')
+    {
+        $result = parent::delete($type, $reason = '');
+
+        $ticket = $this->ticket;
+
+        // Fetch resource and change `ticket_id` to null (0)
+        // TODO: use entity relationship here
+        $resource = \XF::finder('XFRM:ResourceItem')->where('ticket_id', $ticket->ticket_id)->fetchOne();
+
+        if($resource->exists()){
+            $resource->fastUpdate('ticket_id', 0);
+        }
+
+        return $result;
+    }
+}
